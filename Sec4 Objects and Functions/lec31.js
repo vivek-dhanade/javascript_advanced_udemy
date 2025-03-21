85b9cefdccbd03d60b49dfcbbe90735f6154aa7f
Vivek <vivek01110110@gmail.com>
2025-03-21 10:50:36 +0530

    added some files

---
+++ Sec4 Objects and Functions/lec31.js
@@ -0,0 +1,10 @@
+var person = new Object();
+
+person["firstName"] = "Tony";
+person["lastName"] = "Alicea";
+
+var firstNameProperty = "firstName";
+
+console.log(person);
+console.log(person[firstNameProperty]);
+console.log(person.firstName);
